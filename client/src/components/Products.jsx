38d4f1ccb24c9c3090a2643ae7e710f39627949b
Johnydavid <dropin2johny@gmail.com>
2023-09-29 20:55:12 +0530

    c

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import data from "../Data.json"
+import data from "../Data.json";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./Products.css";
-import Image from 'react-bootstrap/Image';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Badge from "react-bootstrap/Badge";
 
 const Products = (props) => {
   return (
@@ -14,52 +15,41 @@ const Products = (props) => {
       <h2>Products </h2>
 
       <Container>
-     
-      <Row>
-    
-        <div className="row text-center  row-cols-md-4 g-4 ">
-    
-  
-      
+        <Row>
+          <div className="row text-center  row-cols-md-4 g-4 ">
+            {props.data.map((item, index) => {
+              return (
+                <div key={index}>
+                  <Card>
+                    <h4 className="cardText">{item.cartText} </h4>
 
-        {props.data.map((item, index) => {
-          return (
-            <div key={index}>
-              <Card >
-              <h4 className="cardText">{item.cartText} </h4>
-           
-                <Card.Img
-                  variant="top"
-                  src={item.imageSrc}
-                  alt={item.imageAlt}
-                  className="cardImg"
-                />
+                    <Card.Img
+                      className="cardImg"
+                      variant="top"
+                      src={item.imageSrc}
+                      alt={item.imageAlt}
+                    />
 
-                <Card.Body>
-                  <Card.Title>{item.cardName}</Card.Title>
-              
-                  <Card.Text>
-               
-                  <badge className ="badgeClass">{item.price}</badge>
-                  </Card.Text>
-                 
-                  <Button variant="primary">Add to Cart</Button>
-                  
-                </Card.Body>
-              </Card>
-            </div>
-          );
-        })}
-         
-      </div>
-        
-        
-      
-       
-      </Row>
-    </Container>
-      
-      
+                    <Card.Body>
+                      {/* <Card.Title>{item.cardName}</Card.Title> */}
+
+                      <Card.Text>
+                        <h2>
+                          <Badge bg="dark">{item.price}</Badge>
+                        </h2>
+
+                        {/* <badge className ="badgeClass">{item.price}</badge> */}
+                      </Card.Text>
+
+                      <Button variant="primary">Add to Cart</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })}
+          </div>
+        </Row>
+      </Container>
     </div>
   );
 };
